46846d51b9fe37a6d6826e3135d74b7539733556
hugoruiz00 <hugo.ruiz.pz@gmail.com>
2023-05-29 20:33:19 -0600

    Configuring react navigation

--- App.js
+++ App.js
@@ -6,14 +6,29 @@
  */
 
 import React from 'react';
-import {  SafeAreaView, StyleSheet,  View} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ShapeDataForm from './src/features/shape/ShapeDataForm';
 import Shape from './src/features/shape/Shape';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <SafeAreaView style={{backgroundColor: 'white', flex: 1}} >
-      <Shape></Shape>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="ShapeDataFrom"
+          component={ShapeDataForm}
+          options={{headerShown:false}}
+        />
+        <Stack.Screen
+          name="Shape"
+          component={Shape}
+          options={{headerShown:false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
